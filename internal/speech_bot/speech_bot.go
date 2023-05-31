0e9c2584654aafdaca70a9a9fbd5318c0eddbbc2
info6e3 <info6e3@gmail.com>
2023-05-31 04:57:35 +0300

    Added modules system

---
+++ internal/speech_bot/speech_bot.go
@@ -0,0 +1,89 @@
+package speech_bot
+
+import (
+	"encoding/json"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+	"main/pkg/download_file"
+	"main/pkg/speech_to_text"
+)
+
+func NewSpeechBot(token string, apiToken string, noneStop bool) *SpeechBot {
+	return &SpeechBot{
+		token:    token,
+		apiToken: apiToken,
+		noneStop: noneStop,
+	}
+}
+
+type SpeechBot struct {
+	token    string
+	apiToken string
+	noneStop bool
+	modules  []func(bot *tgbotapi.BotAPI, message *tgbotapi.Message) error
+}
+
+func (sb *SpeechBot) Run() {
+	var err error
+
+	bot, err := tgbotapi.NewBotAPI(sb.token)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if sb.noneStop {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Println(r)
+				bot.StopReceivingUpdates()
+				sb.Run()
+			}
+		}()
+	}
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.Voice != nil {
+			file, _ := bot.GetFile(tgbotapi.FileConfig{
+				FileID: update.Message.Voice.FileID,
+			})
+
+			filePath := "voices/" + update.Message.Voice.FileUniqueID + ".ogg"
+			download_file.DownloadFile(file.Link(sb.token), filePath)
+
+			answer := speech_to_text.SpeechToText(sb.apiToken, filePath)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, answer)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			logSpeechMessage(update.Message, answer)
+			bot.Send(msg)
+		}
+	}
+}
+
+func logSpeechMessage(message *tgbotapi.Message, VoiceText string) {
+	jsonMessage, err := json.Marshal(message)
+	if err == nil {
+		log.Println(string(jsonMessage))
+		log.Println(VoiceText)
+	}
+}
+
+func (sb *SpeechBot) NewSpeechBot(token string, apiToken string, noneStop bool) *SpeechBot {
+	return &SpeechBot{
+		token:    token,
+		apiToken: apiToken,
+		noneStop: noneStop,
+	}
+}
---
+++ pkg/bot_middlewares/gleb_khalitov_ban.go
@@ -0,0 +1,35 @@
+package bot_middlewares
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"math/rand"
+	"time"
+)
+
+const glebId = 692528838
+
+func GlebKhalitovBan(bot *tgbotapi.BotAPI, message *tgbotapi.Message) error {
+	if message.From.ID == glebId {
+		if message.Voice != nil && poshelNah() {
+			msg := tgbotapi.NewMessage(message.Chat.ID, "пошел ты нах*** Глеб Халитов")
+			msg.ReplyToMessageID = message.MessageID
+			bot.Send(msg)
+		}
+		return &GlebKhalitovError{}
+	}
+	return nil
+}
+
+func poshelNah() bool {
+	rand.Seed(time.Now().UnixNano())
+	if rand.Intn(5) > 0 {
+		return false
+	}
+	return true
+}
+
+type GlebKhalitovError struct{}
+
+func (*GlebKhalitovError) Error() string {
+	return "Это Глеб Халитов!"
+}
